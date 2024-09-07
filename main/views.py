9fcc2d44d581dc2ead2a5ac4ed411d08ee1558e8
Felixsimbolon <felixsmbln25@gmail.com>
2024-09-07 14:06:36 +0700

    tambahan dalam desain serta pergantian dalam file views

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'Name' : 'Gitar Michael Jackson',
+        'Price': 6000000,
+        'Description': 'This was the guitar Michael Used on his tour in early 2009 months before he died'
     }
 
     return render(request, "main.html", context)
