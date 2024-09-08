1d07ff9ef0f0f5d966410a2aa7193d9b17439c53
Felixsimbolon <felixsmbln25@gmail.com>
2024-09-09 03:07:22 +0700

    ganti url gambar

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ def show_main(request):
         'Name' : 'Jeremi Felix Adiyatma',
         'Npm' : '2306219575',
         'Kelas' : 'PBP B',
-        'Item Name' : 'Gitar Michael Jackson',
+        'Item' : 'Gitar Michael Jackson',
         'Price': 6000000,
         'Description': 'This was the guitar Michael Used on his tour in early 2009 months before he died'
     }
