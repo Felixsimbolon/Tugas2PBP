b4d98f74e248b84bd369dd8f5161395ccd515fc0
Felixsimbolon <felixsmbln25@gmail.com>
2024-09-11 09:57:30 +0700

    perubahan pada models

--- main/models.py
+++ main/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
-class MoodEntry(models.Model):
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+class Product(models.Model):
+    item = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+   
--- main/views.py
+++ main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 def show_main(request):
     context = {
-        'Name' : 'Jeremi Felix Adiyatma',
-        'Npm' : '2306219575',
-        'Kelas' : 'PBP B',
-        'Item' : 'Gitar Michael Jackson',
-        'Price': 6000000,
-        'Description': 'This was the guitar Michael Used on his tour in early 2009 months before he died'
+        'name' : 'Jeremi Felix Adiyatma',
+        'npm' : '2306219575',
+        'kelas' : 'PBP B',
+        'item' : 'Gitar Michael Jackson',
+        'price': 6000000,
+        'description': 'This was the guitar Michael Used on his tour in early 2009 months before he died'
     }
 
     return render(request, "main.html", context)
