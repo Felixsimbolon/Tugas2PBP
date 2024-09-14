96972b9efc7506528f6175b96623b272f1a583a6
Felixsimbolon <felixsmbln25@gmail.com>
2024-09-15 02:41:28 +0700

    added jpegs and change in urls and views

--- main/views.py
+++ main/views.py
@@ -9,9 +9,7 @@ def show_main(request):
         'name' : 'Jeremi Felix Adiyatma',
         'npm' : '2306219575',
         'kelas' : 'PBP B',
-        'item' : 'Gitar Michael Jackson',
-        'description': 'This was the guitar Michael Used on his tour in early 2009 months before he died',
-        'satisfaction': product_entries
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
@@ -28,12 +26,12 @@ def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 def show_xml_by_id(request, id):
-    data = MoodEntry.objects.filter(pk=id)
+    data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 def show_json_by_id(request, id):
-    data = MoodEntry.objects.filter(pk=id)
+    data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 # Create your views here.
