2c21e0557bd8bbd7f02092528e3514698ed3f99e
Felixsimbolon <felixsmbln25@gmail.com>
2024-09-17 23:39:40 +0700

    added deploy.yml

--- toko_musik_john_lennon/settings.py
+++ toko_musik_john_lennon/settings.py
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://jeremi-felix-tokomusikjohnlennon.pbp.cs.ui.ac.id", "https://jeremi-felix-tokomusikjohnlennon.pbp.cs.ui.ac.id"]
